003a7ae59c2b7771347bc50a12517c9f05d43b25
Flavio Ramos <flavio.souza@ext.hdi.com.br>
2022-08-24 21:57:00 -0300

    WIP: SETTINGS PAGE

--- lib/pages/SettingsPage.dart
+++ lib/pages/SettingsPage.dart
@@ -23,54 +23,51 @@ class SettingsPage extends StatelessWidget {
         automaticallyImplyLeading: false,
       ),
       body: Container(
-        child: Container(
-            width: double.infinity,
-            height: 800,
-            // padding: const EdgeInsets.only(left: 30, top: 30),
-            decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30),
-                    topRight: Radius.circular(30))),
-            child: Row(children: <Widget>[
-              SettingsList(
-                sections: [
-                  SettingsSection(
-                    title: Text('Common'),
-                    tiles: <SettingsTile>[
-                      SettingsTile.navigation(
-                        leading: Icon(Icons.language),
-                        title: Text('Idioma'),
-                        value: Text('English'),
-                      ),
-                      SettingsTile.switchTile(
-                        onToggle: (value) {},
-                        initialValue: true,
-                        leading: Icon(Icons.format_paint),
-                        title: Text('Enable custom theme'),
-                      ),
-                    ],
-                  ),
-                  SettingsSection(
-                    title: Text('App configs'),
-                    tiles: <SettingsTile>[
-                      SettingsTile.navigation(
-                        leading: Icon(Icons.language),
-                        title: Text('Idioma'),
-                        value: Text('English'),
-                      ),
-                      SettingsTile.switchTile(
-                        onToggle: (value) {},
-                        initialValue: true,
-                        leading: Icon(Icons.format_paint),
-                        title: Text('Enable custom theme'),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            ])),
-      ),
+          width: double.infinity,
+          height: 800,
+          // padding: const EdgeInsets.only(left: 30, top: 30),
+          decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30), topRight: Radius.circular(30))),
+          child: Row(children: <Widget>[
+            SettingsList(
+              sections: [
+                SettingsSection(
+                  title: const Text('Common'),
+                  tiles: <SettingsTile>[
+                    SettingsTile.navigation(
+                      leading: Icon(Icons.language),
+                      title: Text('Idioma'),
+                      value: Text('English'),
+                    ),
+                    SettingsTile.switchTile(
+                      onToggle: (value) {},
+                      initialValue: true,
+                      leading: Icon(Icons.format_paint),
+                      title: Text('Enable custom theme'),
+                    ),
+                  ],
+                ),
+                SettingsSection(
+                  title: Text('App configs'),
+                  tiles: <SettingsTile>[
+                    SettingsTile.navigation(
+                      leading: Icon(Icons.language),
+                      title: Text('Idioma'),
+                      value: Text('English'),
+                    ),
+                    SettingsTile.switchTile(
+                      onToggle: (value) {},
+                      initialValue: true,
+                      leading: Icon(Icons.format_paint),
+                      title: Text('Enable custom theme'),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ])),
     );
   }
 }
